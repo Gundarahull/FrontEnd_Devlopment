3fadc772ce9724eed12a83e5589786a94fa0e0b6
Gundarahull <r161731@rguktrkv.ac.in>
2024-11-26 18:44:28 +0530

    Tailwind-Basics

---
+++ TailWind_CSS/02_CLN/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"], //allowing all files...incase of particular we can include like folder name, file name liks that
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
