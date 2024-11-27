cb9ca9c218d025b5ee847acc0582a089620e5383
Gundarahull <r161731@rguktrkv.ac.in>
2024-11-27 13:45:50 +0530

    Positions_BackgroundClasses_Container_Filters_Interactive Classes"

--- TailWind_CSS/02_CLN/tailwind.config.js
+++ TailWind_CSS/02_CLN/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["*"], //allowing all files...incase of particular we can include like folder name, file name liks that
   theme: {
     extend: {},
+    container:{
+      center:true,
+    },
   },
   plugins: [],
 }
