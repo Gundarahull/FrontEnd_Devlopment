8e54e0c4fcb153ec00c1d936ecc6436d93b1ed5e
Gundarahull <r161731@rguktrkv.ac.in>
2024-11-28 13:36:57 +0530

    "Customization-Directive Clasees"

--- TailWind_CSS/02_CLN/tailwind.config.js
+++ TailWind_CSS/02_CLN/tailwind.config.js
@@ -2,11 +2,37 @@
 module.exports = {
   content: ["*"], //allowing all files...incase of particular we can include like folder name, file name liks that
   theme: {
-    extend: {},
-    container:{
-      center:true,
+    //on;y this will valid other will no applicable in the code
+    // screens:{
+    //   sm: '480px',
+    // },
+    extend: {
+      screens: {
+        sm: "480px",
+        ra: "789px",
+      },
+      //customize what do we want
+      width:{
+        "13":"56px"
+      },
+      height:{
+        "13":"56px"
+      },
+      margin:{
+        "13":"56px"
+      },
+      padding:{
+        "13":"56px",
+        '8':"68px"
+      },
+      //spacing will include all things like w,p,m,h
+      spacing:{
+        "13":"56px",
+      }
+    },
+    container: {
+      center: true,
     },
   },
   plugins: [],
-}
-
+};
